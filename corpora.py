a1261420c65b25650de14141bffc19963bd71eb0
nauticalmile <31225253+nautbot@users.noreply.github.com>
2020-05-11 21:10:13 -0500

    Create corpora.py

---
+++ corpora.py
@@ -0,0 +1,18 @@
+self_fuck = ["I guess you can fuck yourself.", "That's just gross."]
+bot_fuck = ["Uh, that's a bot....", "Maybe try an actual person.", "That's a weird fetish."]
+user_not_playing = ["Looks like that user isn't playing."]
+
+# Requires {0} argument for number of seconds
+married_fuck_cooldown = ["You two are married, you need to wait {0} more seconds lmao."]
+dating_fuck_cooldown = ["Slow down there lovebirds, you need to wait {0} more seconds lol."]
+
+# Requires {0.mention} argument for user looking for sex
+wanna_bang = ["Looks like {0.mention} wants to bang.  You down?"]
+
+married_post_coital = ["Boring marital sex.  Alright then."]
+dating_post_coital = ["Ayyy."]
+single_post_coital = ["***HOT***"]
+cheating_post_coital = ["No one got caught.  *NICE*."]
+
+fuck_declined = ["lmao denied!"]
+fuck_timeout = ["Guess you just got blown off.  Better luck next time."]
